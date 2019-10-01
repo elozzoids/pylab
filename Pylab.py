ab11e58f66210f50a70bdbb72f7821a010be904e
Aeytrg1248 <50971508+Aeytrg1248@users.noreply.github.com>
2019-10-01 03:53:00 +0300

    Update Pylab.py

--- Pylab.py
+++ Pylab.py
@@ -1,13 +1,20 @@
 import urllib.request
+import self as self
 from bs4 import BeautifulSoup
-o = urllib.request.urlopen('https://www.citilink.ru/catalog/audio_and_digits/tv/1079079/')
 
-html_text = """<span class="_2a97c9"> 990&nbsp;₽ </span>"""
+class Data_Repository(object):
+    def __init__(self, listik):
+        pass
+    self.listik = []
+    def parse_site(self)
+        o = urllib.request.urlopen('https://www.citilink.ru/catalog/audio_and_digits/tv/1079079/')
+        html = BeautifulSoup(o.read(), "lxml")
 
+        spans = html.find_all('div', {'class': 'price price_break'})
 
-html = BeautifulSoup(o.read(), "lxml")
-
-spans = html.find_all('div', {'class': 'price price_break'})
-
-for span in spans:
-    print(span)
+        for span in spans:
+            print(span)
+    def save_to_file()
+        f = open('list.txt')
+        f
+        f.close()
